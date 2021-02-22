4af803659aff8ea753071db5aa20fa31066813c7
Justin Ethier <justin.ethier@gmail.com>
2021-02-22 12:34:49 -0500

    Initial file

---
+++ package.scm
@@ -0,0 +1,14 @@
+(package
+ (name           popen)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "Justin Ethier")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "POSIX popen")
+ (tags           "Process I/O")
+ (docs           "https://github.com/cyclone-scheme/popen")
+ (test           "test.scm")
+
+ (library
+     (name (cyclone sys popen))
+   (description "Process I/O library")))
